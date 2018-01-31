09a72a3f84e2963ece1903ffca1c71cdb1e4d80d
neifirst <e.mcculloch.arch@gmail.com>
2018-01-31 11:24:49 +0000

    added hidden word tasts and class

---
+++ hidden_word.rb
@@ -0,0 +1,12 @@
+class HiddenWord
+
+  attr_accessor :answer
+
+  def initialize(answer)
+
+    @answer = answer
+
+  end
+
+
+end
---
+++ specs/hidden_word_spec.rb
@@ -0,0 +1,21 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../hidden_word.rb")
+
+class TestHiddenWord < MiniTest::Test
+
+  def setup
+
+    @hidden_word = HiddenWord.new("cheese")
+
+  end
+
+
+  def test_hidden_word_has_string
+
+    assert_equal("cheese", @hidden_word.answer())
+
+  end
+
+end
